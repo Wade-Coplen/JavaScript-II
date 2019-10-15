17d5e73d5fa283060136bf33a79df7b8673de37d
wade coplen <wadecoplen@gmail.com>
2019-10-14 18:08:25 -0700

    Made sure all is updated

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,8 +1,8 @@
+
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
   // GIVEN THIS PROBLEM:
 
@@ -34,10 +34,12 @@
     return `this ${article} is worth a million dollars!`;
   };
 
+
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
-*/
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
+
+ 
 
 function getLength(arr, cb) {
   cb(array[0]);
@@ -59,29 +61,51 @@ function getLength(arr, cb) {
 
 
 
-function last(arr, cb) {
-  items(arr.length -1);
-  // last passes the last item of the array into the callback.
-}
-  last
+  // function showLast(arr, cb) {
+  // cb(arr.length -1);
+    // last passes the last item of the array into the callback.
+  // }
+  // function showLength(arr, cb) {
+  //   cb(arr.length);
+  // }
+  // showLast(items, (lastItem) => {
+  //   console.log(lastItem);
+  // });
+  // showLast(items, (lastItemInArray) => {
+  //   console.log(lastItemInArray);
+  // });
 
-function sumNums(x, y, cb) {
+
+
+
+
+// function sumNums(x, y, cb) {
+//  return cb(x, y)
+// }
+// let add = (x, y) => x + y
+// console.log(sumNums(2, 4, add))
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
 
-function multiplyNums(x, y, cb) {
+
+// function multiplyNums(x, y, cb) {
+//    return cb(x, y);
+// }
+// let multiply = (x, y) => x * y
+// console.log(multiplyNums(10, 50, multiply))
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
+
+  function contains(item, list, cb) {
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
+  }
+    
+   
+
+  
+  
+  
+  
+ // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
 
-/* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
